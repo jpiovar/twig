39814f948e871148a45a92eae029a1fbd90bcd10
jpiovar <jupi61@gmail.com>
2016-10-20 21:30:10 +0200

    twig php mysqli lib db prepared

---
+++ twig3/index.php
@@ -0,0 +1,118 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+
+$twig = new Twig_Environment($loader);
+
+
+
+$md5Filter = new Twig_simpleFilter('md5', function($string){
+	return md5($string);
+});
+
+$twig->addFilter($md5Filter);
+
+
+$upperFilter = new Twig_simpleFilter('upF', function($string){
+	return strtoupper($string);
+});
+
+$twig->addFilter($upperFilter);
+
+
+/*
+
+$tempVar = array('tt'=>'ttval');
+
+
+echo $twig->render('hello.html', array(
+	'name' => 'John',
+	'age' => 32,
+
+	'users' => array(
+		array('name' => 'Janko', 'surname' => 'Hrasko'),
+		array('name' => 'Jan', 'surname' => 'Hras'),
+		array('name' => 'Jopo', 'surname' => 'Hrach')
+	)
+));
+*/
+
+
+
+
+
+include("./lib/db.php");
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "twig";
+
+
+$dbTwig = new Db;
+
+$dbconnect = $dbTwig->connect($servername,$username,$password,$dbname);
+
+$dboutput = $dbTwig->showAll($dbconnect,"janko");
+
+echo("<br/>mysqli db controller output START <br/>");
+var_dump($dboutput);
+echo("<br/>mysqli db controller output END<br/>");
+
+
+echo $twig->render('hello.html', ['users' => $dboutput]);
+
+
+
+
+
+
+
+
+/*
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+
+
+$sql ="INSERT INTO `twig`.`users` (`id`, `name`, `surname`, `info`) VALUES (NULL, 'jozko1', 'mrkvicka1', '8881');";
+$result = $conn->query($sql);
+
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if($result){
+	echo("<p>controller mysqli db output START</p>");
+	
+	while($row = $result->fetch_assoc()) {
+		
+		echo "<h1>" . $row["id"]. "</h1>"; 
+		
+	}
+	
+	echo("<p>controller mysqli db output END</p>");
+}
+	
+echo $twig->render('hello.html', ['users' => $result]);
+*/
+
+
+
+
+
+
+
+
+
+
+?>
