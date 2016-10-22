a2c29365a2bb22ee2dc9da4140a7d51af04f7899
jpiovar <jpiovar@sk.ibm.com>
2016-10-23 01:02:01 +0200

    webworker progress

--- twig_angular_routeProvider_phpSession/webworker/sessionChecker.js
+++ twig_angular_routeProvider_phpSession/webworker/sessionChecker.js
@@ -4,7 +4,25 @@
 
 function timedCount() { debugger;
     
-    //angular.element(0).scope().sesProcess('11','set');
+    
+	
+	
+	
+	var http = new XMLHttpRequest();
+var url = "../dblib_exec/sesprocess.php";
+var params = "sesok=33&sesoperation=set";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
+	
     setTimeout("timedCount()",500);
 }
 
